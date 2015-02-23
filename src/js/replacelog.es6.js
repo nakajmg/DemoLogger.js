8b1a7533690b2fff364755c477b747f8fd6833d0
nakajmg <nakajima.jmg@gmail.com>
2015-02-24 03:22:22 +0900

    元のconsoleを殺さない

--- src/js/replacelog.es6.js
+++ src/js/replacelog.es6.js
@@ -1,5 +1,7 @@
 var replacelog = (el) => {
+  var _log = console.log;
   console.log = (...args) => {
+    _log.apply(console, args);
     var logs = [];
     for (var i = 0, leng = args.length; i < leng; i++) {
       if (typeof args[i] == 'object') {
