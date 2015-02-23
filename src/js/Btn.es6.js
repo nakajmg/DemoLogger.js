a520a384a1eab075a313b607f54e6dfe66392527
nakajmg <nakajima.jmg@gmail.com>
2015-02-23 18:13:33 +0900

    change Btn style

--- src/js/Btn.es6.js
+++ src/js/Btn.es6.js
@@ -6,9 +6,8 @@ class Btn extends Elem {
       textAlign: 'center',
       color: '#fff',
       display: 'inline-block',
-      // backgroundColor: '#333',
-      backgroundColor: '#C7243A',
-      // border: '1px solid #222',
+      // backgroundColor: '#C7243A',
+      backgroundColor: 'rgb(51, 51, 51)',
       border: 'none',
       borderRadius: '1px',
       padding: '0 10px',
@@ -16,7 +15,6 @@ class Btn extends Elem {
       fontSize: '14px',
       webkitAppearance: 'none',
       boxShadow: 'inset 0 -1px 0 0 rgba(255,255,255,.2)',
-      // margin: '3px 3px 0 0'
       margin: '0'
     };
     
