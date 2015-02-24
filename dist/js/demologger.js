fa152949ac7e5519ea448435562c4919de886c2a
nakajmg <nakajima.jmg@gmail.com>
2015-02-25 04:54:52 +0900

    update lib

--- dist/js/demologger.js
+++ dist/js/demologger.js
@@ -185,7 +185,8 @@ var Frame = (function (Elem) {
       backgroundColor: "#f9faf9",
       padding: "5px",
       borderRadius: "1px",
-      boxShadow: "inset 1px 1px 0 rgba(0,0,0,.2)"
+      boxShadow: "inset 1px 1px 0 rgba(0,0,0,.2)",
+      marginTop: "10px"
     };
 
     _get(Object.getPrototypeOf(Frame.prototype), "constructor", this).call(this, opt);
